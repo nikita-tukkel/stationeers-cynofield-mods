8b92785afe9e2b6c64c2cd2c0b933b0164fa2d11
cynofield <cynical2006@gmail.com>
2022-12-26 21:33:36 +0300

    draft

--- mod1332/Scripts/ModScriptExample.cs
+++ mod1332/Scripts/ModScriptExample.cs
@@ -12,12 +12,14 @@ using Stationeers.Addons;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 namespace cynofield.mods
 {
     public class AugmentedReality : IPlugin
     {
         public static AugmentedReality Instance;
+        public ThingsUi thingsUi;
 
         void IPlugin.OnLoad()
         {
@@ -26,6 +28,7 @@ namespace cynofield.mods
             WorldManager.OnWorldStarted += OnWorldStartedHandler;
             WorldManager.OnWorldExit += OnWorldExitHandler;
 
+            thingsUi = new ThingsUi();
             Instance = this;
             AugmentedDisplayRight.Create();
             AugmentedDisplayInWorld.Create();
@@ -36,6 +39,7 @@ namespace cynofield.mods
         {
             WorldManager.OnWorldStarted -= OnWorldStartedHandler;
             WorldManager.OnWorldExit -= OnWorldExitHandler;
+            thingsUi.Destroy();
             Instance = null;
             AugmentedDisplayRight.Destroy();
             AugmentedDisplayInWorld.Destroy();
@@ -104,12 +108,12 @@ namespace cynofield.mods
 
             if (lookingAt != thing)
             {
-                AugmentedDisplayInWorld.Instance.Show();
                 lookingAt = thing;
                 if (lookingAt != null)
                 {
+                    var desc = thingsUi.Description2d(lookingAt);
                     AugmentedDisplayRight.Instance.Display(
-                        $"<color=white><color=green><b>eyes on</b></color>: {Describe(lookingAt)}</color>");
+                        $"<color=white><color=green><b>eyes on</b></color>: {desc}</color>");
                 }
                 else
                 {
@@ -128,14 +132,39 @@ namespace cynofield.mods
                 pointingAt = thing;
                 if (pointingAt != null)
                 {
+                    var desc = thingsUi.Description2d(pointingAt);
                     AugmentedDisplayRight.Instance.Display(
-                        $"<color=white><color=green><b>mouse on</b></color>: {Describe(pointingAt)}</color>");
+                        $"<color=white><color=green><b>mouse on</b></color>: {desc}</color>");
                 }
                 else
                 {
                     AugmentedDisplayRight.Instance.Hide();
                 }
             }
+        }
+
+    }
+
+    public class ThingsUi
+    {
+        public void RenderARFully(Thing thing, Canvas canvas)
+        {
+
+        }
+
+        public void RenderARText(Thing thing, TextMeshProUGUI textMesh)
+        {
+
+        }
+
+        public string Description2d(Thing thing)
+        {
+            return Describe(thing);
+        }
+
+        public void Destroy()
+        {
+
         }
 
         public static string Describe(Thing thing)
@@ -183,140 +212,149 @@ $@"{t.DisplayName}
         {
             if (Instance == null)
                 return;
-            UnityEngine.Object.Destroy(Instance.obj);
+
+            foreach (var ann in Instance.annotations)
+            {
+                UnityEngine.Object.Destroy((ann as InWorldAnnotation).gameObject);
+            }
             UnityEngine.Object.Destroy(Instance.gameObject);
             Instance = null;
         }
 
-        GameObject obj;
+        public class InWorldAnnotation : MonoBehaviour
+        {
+            GameObject obj;
+            TextMeshProUGUI text;
+            Thing anchor;
+
+            public string id;
+
+            void Start()
+            {
+                ConsoleWindow.Print($"InWorldAnnotation Start");
+                obj = new GameObject("0");
+                obj.SetActive(false);
+                //obj.transform.parent = Instance.gameObject.transform;
+                obj.transform.parent = gameObject.transform;
+                var canvas = obj.AddComponent<Canvas>();
+                canvas.renderMode = RenderMode.WorldSpace;
+
+                var canvasTransform = canvas.transform;
+                canvasTransform.localScale = Vector3.one * 0.5f; // less than 0.5 looks bad
+
+                var bkgd = new GameObject("1").AddComponent<RawImage>();
+                bkgd.rectTransform.SetParent(canvas.transform, false);
+                bkgd.rectTransform.sizeDelta = new Vector2(1f, 1f);
+                bkgd.color = new Color(0.3f, 0.3f, 0.7f, 0.2f);
+
+                text = new GameObject("2").AddComponent<TextMeshProUGUI>();
+                text.rectTransform.SetParent(canvas.transform, false);
+                text.rectTransform.sizeDelta = bkgd.rectTransform.sizeDelta;
+                text.alignment = TextAlignmentOptions.TopLeft;
+                text.fontSize = 0.08f;//0.15f;
+                text.alpha = 0.2f; // low alpha is used to hide font antialiasing artifacts.
+                text.color = new Color(0f, 0f, 0f, 0.9f);
+                text.richText = true;
+                text.margin = new Vector4(0.05f, 0.05f, 0.05f, 0.05f);
+            }
+
+            public void ShowNear(Thing thing, string id, string desc)
+            {
+                if (thing == null)
+                {
+                    Hide();
+                    return;
+                }
+                if (thing == anchor)
+                {
+                    return;
+                }
+                this.anchor = thing;
+                this.id = id;
+
+                // relink to new parent, thus appear in the parent scene.
+                transform.SetParent(thing.transform, false);
+                transform.SetPositionAndRotation(
+                    // need to reset position on this component reuse
+                    thing.transform.position + Vector3.zero,
+
+                    // rotate vertically to the camera:
+                    Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0)
+                    );
+
+                var pos = transform.position;
+                var posPlayer = InventoryManager.ParentHuman.transform.position;
+                transform.position = new Vector3(pos.x, posPlayer.y + 0.8f, pos.z);
+                transform.Translate(Camera.main.transform.forward * -0.5f, Space.World);
+                text.text = $@"{thing.NetworkId} {thing.DisplayName}
+{desc}
+please <color=red><b>don't</b></color> play with me";
+                obj.SetActive(true);
+                gameObject.SetActive(true);
+            }
+
+            public void Hide()
+            {
+                obj.SetActive(false);
+            }
+        }
+
+        InWorldAnnotation ann;
+        Queue annotations = new Queue();
 
         void Start()
         {
-            ConsoleWindow.Print($"AugmentedDisplayLeft Start {gameObject} {this}");
-            obj = new GameObject("0");
-            obj.transform.parent = Instance.gameObject.transform;
-            var canvas = obj.AddComponent<Canvas>();
-            canvas.renderMode = RenderMode.WorldSpace;
-
-            var canvasTransform = canvas.transform;
-            //canvasTransform.position = new Vector3(-1f, 3.05f, 2.5f);
-            //canvasTransform.position = new Vector3(-100f, 100f, 0f);
-            //canvasTransform.localPosition = new Vector3(0f, 0.5f, 0f);
-            //canvasTransform.rotation = Quaternion.Euler(0.1f, 1f,1f);
-            //canvas.pixelPerfect
-            canvasTransform.localScale = Vector3.one * 0.5f; //*5f;// Vector3.one;
-
-            var bkgd = new GameObject("1").AddComponent<RawImage>();
-            bkgd.rectTransform.SetParent(canvas.transform, false);
-            bkgd.rectTransform.sizeDelta = new Vector2(2f, 2f);
-            bkgd.color = new Color(1f, 1f, 1f, 0.05f);
-
-            var text = new GameObject("2").AddComponent<TextMeshProUGUI>();
-            text.rectTransform.SetParent(canvas.transform, false);
-            text.rectTransform.sizeDelta =bkgd.rectTransform.sizeDelta;
-            text.alignment = TextAlignmentOptions.TopLeft;
-            text.fontSize = 0.15f;//0.15f;
-            text.alpha = 0.5f; // low alpha is used to hide font antialiasing artifacts.
-            text.color = Color.white;// new Color(1f, 1f, 1f, 0.15f);// Color.white;
-            text.richText = true;
-            text.text = "please <color=red><b>don't</b></color> play with me";
-
-            // TextMeshProUGUI textMeshProUgui = gameObject.AddComponent<TextMeshProUGUI>();
-
-            // textMeshProUgui.rectTransform.SetParent(bkgd.transform, false);
-            // textMeshProUgui.rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
-            // textMeshProUgui.rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
-            // textMeshProUgui.rectTransform.sizeDelta = new Vector2(1f, 1f);
-            // textMeshProUgui.rectTransform.transform.localPosition = Vector3.zero;
-            // textMeshProUgui.rectTransform.anchoredPosition = new Vector2(0f, 0f);
-            // textMeshProUgui.transform.localPosition = new Vector3(0f, 0.3f, -0.5f);
-
-            // textMeshProUgui.richText = true;
-            // textMeshProUgui.text = "please <color=red><b>don't</b></color> play with me";
-            // textMeshProUgui.fontSize = 0.15f;//0.15f;
-            // textMeshProUgui.color = Color.white;// new Color(1f, 1f, 1f, 0.15f);// Color.white;
-            // textMeshProUgui.alignment = TextAlignmentOptions.Left;
-            // textMeshProUgui.alpha = 0.5f; // low alpha is used to hide font antialiasing artifacts.
+            ConsoleWindow.Print($"AugmentedDisplayInWorld Start");
+            annotations.Enqueue(new GameObject().AddComponent<InWorldAnnotation>());
+            annotations.Enqueue(new GameObject().AddComponent<InWorldAnnotation>());
         }
 
-        public void Show()
+        void Update()
         {
-            if (CursorManager.Instance == null || CursorManager.Instance.FoundThing == null)
+            bool isCtrlKeyDown = Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
+            if (isCtrlKeyDown)
+            {
+                foreach (var obj in Instance.annotations)
+                {
+                    (obj as InWorldAnnotation).gameObject.SetActive(false);
+                }
                 return;
-            var th = Assets.Scripts.CursorManager.Instance.FoundThing;
-            if (th.transform == null)
+            }
+
+            if (CursorManager.Instance == null || CursorManager.CursorThing == null)
+            {
                 return;
-            if (obj == null)
+            }
+
+            bool isShiftKeyDown = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+            if (!isShiftKeyDown)
                 return;
 
-            // All options requires different values of:
-            // canvasTransform.localScale
-            // textMeshProUgui.fontSize
-            // textMeshProUgui.alpha 
-
-            // when relative to InventoryManager.ParentHuman.transform
-            // x - left/right, 0.2 is ok to show on the right part of the screen
-            // y - up/down, depends on human height, 1 is ok.
-            // z - how close, 0.3 is almost at the helmet glass.
-            // gameObject.transform.SetParent(InventoryManager.ParentHuman.transform, false);
-            // gameObject.transform.localPosition = new Vector3(0.2f,1,0.3f);
-
-            // when relative to InventoryManager.ParentHuman.GlassesSlot.Occupant.transform,
-            // also need to sync rotation with Camera.main.transform.rotation for better result.
-            // var glassesTransform = InventoryManager.ParentHuman.GlassesSlot.Occupant.transform;
-            // gameObject.transform.SetParent(glassesTransform, false);
-            // gameObject.transform.localPosition = new Vector3(0.2f,0.2f,0.2f);
-            // gameObject.transform.rotation = Camera.main.transform.rotation;
-
-            // when relative to Camera.main.transform, but in this case better use 2d GUI.
-            // var cameraTransform = Camera.main.transform;
-            // gameObject.transform.SetParent(cameraTransform, false);
-            // gameObject.transform.localPosition = new Vector3(0.2f,0.1f,0.15f);
-
-            obj.transform.SetParent(th.transform, false);
-            obj.transform.position = th.transform.position + Vector3.zero;
-            //obj.transform.localPosition = new Vector3(0.2f, 0.1f, 1f);
-            var tr = th.transform.rotation;
-            var cr = Camera.main.transform.rotation;
-            var r = cr.normalized;
-
-            // rotate vertically to the camera:
-            obj.transform.rotation = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0); ;
-
-            var pos = obj.transform.position;
-            var posPlayer = InventoryManager.ParentHuman.transform.position;
-            obj.transform.position = new Vector3(pos.x, posPlayer.y + 0.8f, pos.z);
-            obj.transform.Translate(Camera.main.transform.forward * -0.5f, Space.World);
-
-            ConsoleWindow.Print($"our position {obj.transform.position}, parent position {th.transform.position}");
-
-            //th.transform.Translate(Vector3.forward);
-            //th.transform.localScale = Vector3.one * 0.5f;
-            // Vector3 look = Camera.main.transform.TransformDirection(Vector3.forward);
-            // Debug.DrawRay(Camera.main.transform.position, look, Color.green, 14);
-            // var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            // cube.transform.position = th.transform.position;
-            // Destroy(cube);
-            // var plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-            // plane.transform.position = th.transform.position;
-
-
-            // var pos = gameObject.transform.position;
-            // gameObject.transform.position = new Vector3(pos.x   ,pos.y, pos.z);
-            //gameObject.transform.position = new Vector3(-70f   ,-0.7f, 3);
-
-
-            // var b = gameObject.GetComponentsInChildren<TextMeshProUGUI>();
-
-            // var pos = gameObject.transform.position;
-            // pos = new Vector3(pos.x, pos.y+2, pos.z);
-            // var a = gameObject.GetComponentsInChildren<Canvas>();
-            //ConsoleWindow.Print($"AugmentedDisplayLeft Show {pos} {a.Length} {b.Length}");
-            // var pos2 = b[0].transform.position;
-            // b[0].transform.position = new Vector3(pos2.x, pos2.y+0.2f, pos2.z);
-
-            obj.SetActive(true);
+            var thing = CursorManager.CursorThing;
+            var desc = AugmentedReality.Instance.thingsUi.Description2d(thing);
+            AugmentedDisplayInWorld.Instance.Show(thing, desc);
         }
+
+        public void Show(Thing thing, string desc)
+        {
+            if (thing == null)
+                return;
+
+            var thingId = GetId(thing);
+
+            foreach (var obj in Instance.annotations)
+            {
+                // return if there is already shown annotation for this thing
+                if ((obj as InWorldAnnotation).id == thingId)
+                    return; // TODO update description?
+            }
+
+            var ann = annotations.Dequeue() as InWorldAnnotation;
+            annotations.Enqueue(ann);
+            ann.ShowNear(thing, thingId, desc);
+        }
+
+        string GetId(Thing thing) { return thing.NetworkId.ToString(); }
     }
 
     public class AugmentedDisplayRight : MonoBehaviour
@@ -364,17 +402,16 @@ $@"{t.DisplayName}
             var h = Screen.height - y - 300;
             GUI.Box(new Rect(x, y, w, h), "");
             GUI.color = new Color(1.0f, 1.0f, 1.0f, 0.25f);
-            GUI.Label(new Rect(x, y, w, h), "aaa Loaded");
 
             //GUI.Label(new Rect(x, y, w, h), text);
-            // GUI.Box(new Rect(x, y, w, h), text
-            // ,
-            // new GUIStyle()
-            // {
-            //     alignment = TextAnchor.UpperLeft,
-            //     richText = true,
-            //     fontSize = 16
-            // });
+            GUI.Box(new Rect(x, y, w, h), text
+            ,
+            new GUIStyle()
+            {
+                alignment = TextAnchor.UpperLeft,
+                richText = true,
+                fontSize = 16
+            });
             GUI.Box(new Rect(x, y + 100, w, h),
             $@"<color=grey>
 aaa Loaded
